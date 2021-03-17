12574400d71e418af8954afb343356c70523a8dd
changyuanliu <changyuan@ustc.edu>
2021-03-17 18:35:23 -0400

    Paremeter change

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -24,7 +24,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
-MAX_DECEL = 4.0
+MAX_DECEL = 2.0
 
 class WaypointUpdater(object):
     def __init__(self):
